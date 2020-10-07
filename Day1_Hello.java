5fbae19ee0a989b1cf159bdea9052d90a3cfde59
aviralsahu <65275071+aviralsahu@users.noreply.github.com>
2020-10-07 12:58:16 +0530

    Add files via upload

---
+++ Day1_Hello.java
@@ -0,0 +1,14 @@
+class Hello {
+	public static void main(String args[])
+		{
+		   System.out.println("Welcome to my Github account");
+		   System.out.println("https://github.com/aviralsahu");
+    }
+}
+
+/*
+
+To compile : javac Hello.java
+To execute : java Hello
+
+*/
